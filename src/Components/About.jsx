dba32570ebdaa3c44019ee236f65ac106bed2bd4
kefaslungu <jameskefaslungu@gmail.com>
2023-08-30 12:37:05 +0100

    'updatedDetails'

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,19 +24,18 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I'm a python, C, and javascript developer studying history and international studies at the university of jos.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Python",
+  "C",
+  "Javascript",
+  "Linux administration",
+  "mysql",
 ];
 
 /**
@@ -45,7 +44,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my expertis I continually look for new and better ways to make tech accessible to all.";
 
 const About = () => {
   return (
@@ -60,7 +59,7 @@ const About = () => {
           textAlign: "center",
         }}
       >
-        <h2>About Myself</h2>
+        <h2>About Kefas Lungu</h2>
         <p className="large">{description}</p>
         <hr />
         <ul
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "System Inspect",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "An attempt to imitate lenovo vantage on systems that don't have it.",
+    url: "https://github.com/kefaslungu/systeminspect",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Resource Monitor",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A handy Resource Monitor addon for the nvda screen reader to announce what? Check for your self!",
+    url: "https://github.com/kefaslungu/resourceMonitor",
   },
   {
-    title: "My Resume Site",
+    title: "Enhancedtouchgestures",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Still another NVDA addon.",
+    url: "https://github.com/kefaslungu/enhancedtouchgestures",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Written so many things on quora!",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "All urls cant be brought here, so I just did it like that.",
+    url: "https://quora.com/users/kefaslungu",
   },
 ];
 
